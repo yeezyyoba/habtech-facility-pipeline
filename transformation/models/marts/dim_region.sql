d2353919d65a13fa7e5894ad6ec004bc849b98dd
Eyob Nebyou <eyobnebyou10@gmail.com>
2026-08-25 12:38:10 +0300

    feat(k8s): integrate Airflow DAGs, Meltano jobs, and dbt models on Minikube

--- transformation/models/marts/dim_region.sql
+++ transformation/models/marts/dim_region.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='table', engine='MergeTree', order_by='region') }}
+{{ config(materialized='table', engine='MergeTree', order_by='region', settings={'allow_nullable_key': 1}) }}
 select distinct region
 from {{ ref('stg_mfr_facilities') }}
 where region is not null
--- transformation/models/marts/dim_zone_woreda.sql
+++ transformation/models/marts/dim_zone_woreda.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='table', engine='MergeTree', order_by='(region, zone, woreda)') }}
+{{ config(materialized='table', engine='MergeTree', order_by='(region, zone, woreda)', settings={'allow_nullable_key': 1}) }}
 select distinct region, zone, woreda
 from {{ ref('stg_mfr_facilities') }}
 where zone is not null
